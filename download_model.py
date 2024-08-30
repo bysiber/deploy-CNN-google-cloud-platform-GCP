497355f48eaf7603d3430cb5dd8b0b007c4f6c53
Can Ozden <101993364+bysiber@users.noreply.github.com>
2024-08-30 23:22:23 +0300

    uploading_project_again

---
+++ download_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import keras
+
+if __name__ == '__main__':
+  #InceptionResNetV2
+  model=keras.applications.inception_resnet_v2.InceptionResNetV2(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+  model.save_weights("./InceptionResNetV2_weights.h5")
+  print("Model is saved")
